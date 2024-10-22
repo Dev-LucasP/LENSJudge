1b09e3f50e6b96c2ccfe29d43b2d4ff1df397d24
nathan.pierreuse <nathan.pierreuse@info0316.ipa.iutlens.local>
2024-10-22 11:05:14 +0200

    :zap: add of the interface IprocessStrategy 2

--- app/src/main/java/lensjuge/process/IProcessStrategy.java
+++ app/src/main/java/lensjuge/process/IProcessStrategy.java
@@ -1,5 +1,15 @@
 package lensjuge.process;
 
-public interface IProcessStrategy {
+public interface IProcessStrategy{
+	
+	public String execute();
+	
+	public Process createProcessus();
+	
+	public void stopProcessus();
+	
+	public String getInput();
+	
+	public String getOutput();
 
 }
