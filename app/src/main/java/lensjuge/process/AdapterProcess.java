3c4a52f77bff24c732384e98c11693f76fbb468c
nathan.pierreuse <nathan.pierreuse@info0316.ipa.iutlens.local>
2024-10-22 11:26:45 +0200

    :zap: add of AdapterProcess

---
+++ app/src/main/java/lensjuge/process/AdapterProcess.java
@@ -0,0 +1,37 @@
+package lensjuge.process;
+
+public class AdapterProcess implements IProcessStrategy{
+
+	@Override
+	public String execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Process createProcessus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void stopProcessus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getInput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOutput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
