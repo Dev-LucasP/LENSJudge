917057a3eafe09612eee7ff61e162347e6d2c9cb
Gomezgonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-10-22 15:24:39 +0200

    :sparkles: :recycle: #14 #17 added test for TimeLimit + refactor the function isTimerOut

--- app/src/main/java/lensjuge/process/ProcessTimer.java
+++ app/src/main/java/lensjuge/process/ProcessTimer.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 public class ProcessTimer {
     private AdapterProcess adapterProcess;
     private long timeOut;
+
     public ProcessTimer(AdapterProcess adapterProcess, long timeOut) {
         this.adapterProcess = adapterProcess;
         this.timeOut = timeOut;
@@ -13,17 +14,21 @@ public class ProcessTimer {
     public boolean isTimedOut() {
         Instant start = Instant.now();
         Instant end = start.plusMillis(timeOut);
-        while (Instant.now().isBefore(end) && adapterProcess.getProcess().isAlive()) {
+        while (Instant.now().isBefore(end)) {
+            if (!adapterProcess.getProcess().isAlive()) {
+                return false;
+            }
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
+                return false;
             }
         }
         if (adapterProcess.getProcess().isAlive()) {
             adapterProcess.getProcess().destroy();
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
