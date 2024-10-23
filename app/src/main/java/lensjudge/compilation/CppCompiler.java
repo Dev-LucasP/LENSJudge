a9410d9e4bf136926998a07f6e74afa8b849f5cd
lucas.perez <lucas.perez@info0317.ipa.iutlens.local>
2024-10-23 09:34:44 +0200

    :sparkles: Add CppCompiler.java #26

--- app/src/main/java/lensjudge/compilation/CppCompiler.java
+++ app/src/main/java/lensjudge/compilation/CppCompiler.java
@@ -2,28 +2,29 @@ package lensjudge.compilation;
 
 import java.io.IOException;
 
-import lensjudge.process.*;
-
 class CppCompiler extends CompilerStrategy {
-	
-	public CppCompiler(String sourceFile) {
-		super(sourceFile);
-	}
 
-	@Override
+    public CppCompiler(String sourceFile) {
+        super(sourceFile);
+    }
+
+    @Override
     protected boolean checkFileExtension() {
         return sourceFile.endsWith(".cpp") || sourceFile.endsWith(".cc") || sourceFile.endsWith(".cxx");
     }
-    
-	@Override
-    protected String deduceBinaryFile() {
+
+    @Override
+    protected String deduceBinaryFile(String sourceFile) {
         return sourceFile.replace(".cpp", ".out").replace(".cc", ".out").replace(".cxx", ".out");
     }
-    
-	@Override
-	public void compile() throws IOException {
-    	AdapterProcess adapterProcess = new AdapterProcess();
-        adapterProcess.createProcessus();
-        adapterProcess.execute(new String[]{"g++", "-x", "c++", "-Wall", "-02", "-static", "-pipe", "-o", "exe", sourceFile});
+
+    @Override
+    public boolean compile() throws IOException, InterruptedException {
+        if (!checkFileExtension()) {
+            throw new IllegalArgumentException("Invalid C++ file extension");
+        }
+
+        String[] command = { "g++", sourceFile, "-o", deduceBinaryFile(sourceFile) };
+        return runCommand(command);
     }
 }
