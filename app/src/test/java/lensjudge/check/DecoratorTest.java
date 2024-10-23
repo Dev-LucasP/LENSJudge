4cd59d7d74a8502c1501501123f14e98dfc28399
Alexandre MIONE <alexandre.mione@univ-artois.fr>
2024-10-23 13:45:52 +0200

    :bug: location fixed for Decorator and adding a test program fot it

---
+++ app/src/test/java/lensjudge/check/DecoratorTest.java
@@ -0,0 +1,21 @@
+package lensjudge.check;
+
+import org.junit.jupiter.api.Test;
+import java.io.StringReader;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DecoratorTest {
+
+    @Test
+    public void testReadLine() throws IOException {
+        String input = "Hello   World\nThis is a TEST";
+        String expectedOutput1 = "hello world";
+        String expectedOutput2 = "this is a test";
+
+        try (Decorator decorator = new Decorator(new StringReader(input))) {
+            assertEquals(expectedOutput1, decorator.readLine());
+            assertEquals(expectedOutput2, decorator.readLine());
+        }
+    }
+}
