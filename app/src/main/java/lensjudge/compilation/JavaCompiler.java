95f515bd80fa8f8b4a9a3be855b5f6b9bf676e24
Gomezgonzalez Enzo <enzo_gomezgonzalez@ens.univ-artois.fr>
2024-10-23 10:27:49 +0200

    :recycle: #22 #27 refactored the code to extends CompilerStrategy

--- app/src/main/java/lensjudge/compilation/JavaCompiler.java
+++ app/src/main/java/lensjudge/compilation/JavaCompiler.java
@@ -1,24 +1,30 @@
 package lensjudge.compilation;
 
-import lensjudge.process.AdapterProcess;
 
 import java.io.IOException;
 
-public class JavaCompiler {
-    private final String sourcePath;
+public class JavaCompiler extends CompilerStrategy {
 
-    public JavaCompiler(String sourcePath) throws IllegalArgumentException {
-        this.sourcePath = sourcePath;
-        if (!sourcePath.endsWith(".java")) {
-            throw new IllegalArgumentException("Source file must end with .java");
-        }
+    public JavaCompiler(String sourceFile) throws IllegalArgumentException {
+        super(sourceFile);
+    }
+
+    @Override
+    protected boolean checkFileExtension() {
+        return sourceFile.endsWith(".java");
+    }
+
+    @Override
+    protected String deduceBinaryFile(String sourceFile) {
+        return sourceFile.replace(".java", ".class");
     }
 
     public boolean compile() throws IOException, InterruptedException {
-        AdapterProcess adapterProcess = new AdapterProcess();
-        adapterProcess.createProcessus();
-        adapterProcess.execute(new String[]{"javac", sourcePath});
-        adapterProcess.getProcess().waitFor();
-        return adapterProcess.getProcess().exitValue() == 0;
+        if (!checkFileExtension()) {
+            throw new IllegalArgumentException("Invalid Java file extension");
+        }
+
+        String[] command = {"javac", sourceFile};
+        return runCommand(command);
     }
 }
